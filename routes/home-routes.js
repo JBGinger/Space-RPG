974e00bf45415cd10728a8f5d8ddb11c49f48b00
minha619 <minha619@gmail.com>
2021-11-30 00:04:41 -0500

    handlebars update

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, Inventory, Item, Planet } = require("../../models");
+const { User, Inventory, Item, Planet } = require("../models");
 
 router.get('/', (req, res) => {
     if (req.session.loggedIn) {
