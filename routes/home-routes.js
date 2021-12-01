44c9b2030004fb7673f3122f2012cf89699db192
minha619 <minha619@gmail.com>
2021-11-30 22:57:58 -0500

    main.handlebars update

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Inventory, Item, Planet } = require("../models");
 
+
 router.get('/', (req, res) => {
     if (req.session.loggedIn) {
       res.redirect('/');
