ef9121aff207e1c31be6ed97e61113927e48a63f
Granade <mark.granade@gmail.com>
2021-12-03 10:08:33 -0500

    finally fixed login validation issues

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,12 +38,12 @@ router.get("/mars", (req, res) => {
 	res.render("mars");
 });
 
-router.get("/login", (req, res) => {
-	if (req.session.loggedIn) {
-		res.redirect("/");
-		return;
-	}
-	res.render("login");
-});
+// router.get("/login", (req, res) => {
+// 	if (req.session.loggedIn) {
+// 		res.redirect("/");
+// 		return;
+// 	}
+// 	res.render("login");
+// });
 
 module.exports = router;
